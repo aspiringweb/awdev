e7dddd159a3353f9cb077015b57bfc5c7769beb1
Matthew Connerton <matthew@aspiringweb.com>
2021-06-08 22:27:00 -0400

    Adds extensions install for vscode.

---
+++ scripts/vscode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+code --install-extension adamwalzer.scss-lint
+code --install-extension DmitryDorofeev.empty-indent
+code --install-extension felixfbecker.php-debug
+code --install-extension ikappas.phpcs
+code --install-extension juniormucciolo.drupal-7-snippets
+code --install-extension marcostazi.VS-code-drupal
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension pauloamgomes.drupal7-hooks-snippets
+code --install-extension persoderlind.vscode-phpcbf
+code --install-extension redhat.vscode-yaml
+code --install-extension usernamehw.errorlens
+code --install-extension whatwedo.twig
